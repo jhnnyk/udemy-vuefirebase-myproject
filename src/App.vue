fda835f4ba04c0a56dd921202439580b6bcffe2b
John Knotts <jhnnyk@gmail.com>
2019-11-06 05:32:19 -0700

    passing data with props

--- src/App.vue
+++ src/App.vue
@@ -2,33 +2,32 @@
   <div id="app">
     <h1>{{ title }}</h1>
     <Navbar />
-    <nav>
-      <ul>
-        <li>
-          <a href>Home</a>
-        </li>
-        <li>
-          <a href>About</a>
-        </li>
-        <li>
-          <a href>Contact</a>
-        </li>
-      </ul>
-    </nav>
+    <AllFriends :friends="friends" />
+    <OnlineFriends :friends="friends" />
   </div>
 </template>
 
 <script>
 import Navbar from "./components/Navbar";
+import AllFriends from "./components/AllFriends";
+import OnlineFriends from "./components/OnlineFriends";
 
 export default {
   name: "app",
   components: {
-    Navbar
+    Navbar,
+    OnlineFriends,
+    AllFriends
   },
   data() {
     return {
-      title: "My first Vue app, woooo :-)"
+      title: "My first Vue app, woooo :-)",
+      friends: [
+        { name: "Mario", online: true },
+        { name: "Luigi", online: false },
+        { name: "Toad", online: true },
+        { name: "Bowser", online: false }
+      ]
     };
   }
 };
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -24,7 +24,7 @@ export default {
 };
 </script>
 
-<style scoped>
+<style>
 nav {
   text-align: center;
 }
