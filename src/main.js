34ad6334b9b169d3627c97497c93f9009cec4c90
John Knotts <jhnnyk@gmail.com>
2019-11-08 08:53:34 -0700

    filters

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+// create global filter
+Vue.filter('snippet', val => {
+  if (!val || typeof val != 'string') {
+    return '';
+  }
+
+  val = val.slice(0, 50);
+  return val;
+});
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: h => h(App)
+}).$mount('#app');
